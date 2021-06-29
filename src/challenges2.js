706a57376d9912f281a1cbf6f22d7f0717a6abc6
Marcello Pires Alves <marcello.alves@gmail.com>
2021-06-29 18:28:22 -0300

    Requisito 11: generatePhoneNumber.

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,8 +12,25 @@ function techList(list, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(digitos) {
   // seu código aqui
+  if (digitos.length !== 11) {
+    return 'Array com tamanho incorreto.'; // se recebeu quantidade diferente de dígitos, retorna mensagem de erro
+  }
+  for (let digit of digitos) {
+    if (digit < 0 || digit > 9) {
+      return 'não é possível gerar um número de telefone com esses valores'; // se as entradas não forem dígitos, retorna mensagem de erro
+    }
+  }
+  for (let i = 0; i <= 9; i += 1) { // a cada passagem, testamos um dígito:
+    if (digitos.filter((x) => (x === i)).length >= 3) { // filter() elimina os demais valores do array, usando a função callback; length contém o número de ocorrências
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  digitos.splice(0, 0, '('); // gera o número de telefone: splice() insere os caracteres
+  digitos.splice(3, 0, ') ');
+  digitos.splice(9, 0, '-');
+  return digitos.join(''); // join() converte o array em string
 }
 
 // Desafio 12
