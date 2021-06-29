a2c47ea741e7cafc3082591a8aed46885c6e4574
Marcello Pires Alves <marcello.alves@gmail.com>
2021-06-29 14:19:19 -0300

    Requisito 2: calcArea.

--- src/challenges.js
+++ src/challenges.js
@@ -5,8 +5,9 @@ function compareTrue(param1, param2) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return (base * height / 2);
 }
 
 // Desafio 3
