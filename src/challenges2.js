c39333a42088c0210ac93d7091fa6aa990b7b221
Marcello Pires Alves <marcello.alves@gmail.com>
2021-06-29 17:41:43 -0300

    Requisito 10: techList.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,14 @@
 // Desafio 10
-function techList() {
+function techList(list, name) {
   // seu código aqui
+  let objList = []; // cria array vazio para receber os objetos
+  list.sort(); // ordena a lista
+  for (let tech of list) {
+    // para cada item de list, acrescenta um objeto
+    objList.push({ tech, name });
+  }
+  // retorna o array, ou 'Vazio!' se o array está vazio
+  return objList.length === 0 ? 'Vazio!' : objList;
 }
 
 // Desafio 11
