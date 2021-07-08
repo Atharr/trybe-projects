d9b746b4dbd3c32613f81b9e0efa8e1915a0fd31
Marcello Pires Alves <marcello.alves@gmail.com>
2021-07-08 16:17:40 -0300

    Requisito 9.

--- script.js
+++ script.js
@@ -2,60 +2,70 @@
 // cada componente pode variar entre 0 e 255
 function randomColor() {
   // gera aleatoriamente cada um dos componentes
-  let r = Math.floor(Math.random() * 256);
-  let g = Math.floor(Math.random() * 256);
-  let b = Math.floor(Math.random() * 256);
-  //retorna uma expressão rgb(r,g,b)
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  // retorna uma expressão rgb(r,g,b)
   return `rgb(${r},${g},${b})`;
 }
 
 // createPalette(): monta a paleta e configura as cores
 // o parâmetro n determina quantas cores são adicionadas à paleta
 function createPalette(n) {
-  // obtém o seletor da #color-palette
-  let palette = document.querySelector("#color-palette");
-  // remove os filhos (colors)
+  const palette = document.querySelector('#color-palette'); // obtém o seletor da #color-palette
   while (palette.firstChild) {
-    palette.removeChild(palette.lastChild);
+    palette.removeChild(palette.lastChild); // remove os filhos (colors)
   }
   // acrescenta as cores à color-palette
-  for (let i = 0; i < parseInt(n); i += 1) {
-    let color = document.createElement("div"); // cria um elemento div
-    color.className = "color"; // atribui a classe .color
-    color.style.backgroundColor = (i === 0 ? "black" : randomColor()); // atribui a cor (o primeiro sempre é black)
-    color.addEventListener("click", (event) => { // acrescenta o event listener
-      document.querySelector(".selected").classList.remove("selected");
-      event.target.classList.add("selected");
+  for (let i = 0; i < n; i += 1) {
+    const color = document.createElement('div'); // cria um elemento div
+    color.className = 'color'; // atribui a classe .color
+    color.style.backgroundColor = (i === 0 ? 'black' : randomColor()); // atribui a cor (o primeiro sempre é black)
+    color.addEventListener('click', (event) => { // acrescenta o event listener
+      document.querySelector('.selected').classList.remove('selected');
+      event.target.classList.add('selected');
     });
     palette.appendChild(color); // anexa a color como filha de color-palette
   }
-  // seleciona a primeira cor
-  document.querySelector(".color").classList.add("selected");
+  document.querySelector('.color').classList.add('selected'); // seleciona a primeira cor
 }
 
 // createBoard(): cria o tabuleiro de pixels
 // o parâmetro n determina o tamanho do tabuleiro (n * n)
 function createBoard(n) {
-  // obtém o seletor do pixel-board
-  let board = document.querySelector("#pixel-board");
-  // remove os filhos (pixels)
+  const board = document.querySelector('#pixel-board'); // obtém o seletor do pixel-board
   while (board.firstChild) {
-    board.removeChild(board.lastChild);
+    board.removeChild(board.lastChild); // remove os filhos (pixels)
   }
-  // ajusta o tamanho do pixel-board
-  let size = n * 41;
+  // ajusta o tamanho do #pixel-board
+  const size = n * 41;
   board.style.width = `${size}px`;
   board.style.height = `${size}px`;
   // acrescenta os pixels ao pixel-board
-  for (let i = 0; i < Math.pow(n, 2); i += 1) {
-    let pixel = document.createElement("div"); // cria um elemento div
-    pixel.className = "pixel"; // atribui a classe .pixel
-    pixel.style.backgroundColor = "white"; // atribui a cor branca
-    pixel.addEventListener("click", (event) => { // acrescenta o event listener
+  for (let i = 0; i < n ** 2; i += 1) {
+    const pixel = document.createElement('div'); // cria um elemento div
+    pixel.className = 'pixel'; // atribui a classe .pixel
+    pixel.style.backgroundColor = 'white'; // atribui a cor branca
+    pixel.addEventListener('click', () => { // acrescenta o event listener
       pixel.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
     });
     board.appendChild(pixel); // anexa o pixel como filho de pixel-board
   }
+}
+
+// setButtonClearBoard(): configura o botão que 'limpa' o tabuleiro de pixels
+// todos os pixels têm a cor alterada para branco
+function setButtonClearBoard() {
+  // acrescenta o event listener ao botão
+  document.querySelector('#clear-board').addEventListener('click', () => {
+    // obtém os filhos de pixel-board (pixels)
+    const pixels = document.querySelector('#pixel-board').children;
+    // altera a cor de cada pixel para branco
+    for (const pixel of pixels) {
+      pixel.style.backgroundColor = 'white';
+    }
+    // pixels.forEach((el, i, arr) => (arr[i].style.backgroundColor = 'white'));
+  });
 }
 
 // executa as funções ao carregar a página
@@ -64,4 +74,6 @@ window.onload = () => {
   createPalette(4);
   // cria o tabuleiro de pixels com tamanho 5 x 5
   createBoard(5);
-}
+  // configura os event listeners dos botões
+  setButtonClearBoard();
+};
