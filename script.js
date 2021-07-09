5214488e679da6b7ed43f3057110c977288fd11d
Marcello Pires Alves <marcello.alves@gmail.com>
2021-07-09 15:24:12 -0300

    Requisito 9.

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // setItemGray(event): atribui a classe .cinza a um elemento
 // a função checa se já existe um elemento com essa classe e a remove
 function setItemGray(event) {
-  // obtém o seletor do item com a classe .cinza
-  const itemcinza = document.querySelector('.cinza');
+  // obtém o seletor do item com a classe .gray
+  const grayitem = document.querySelector('.gray');
   // se existe um item com a classe aplicada, remove-a
-  if (itemcinza) {
-    itemcinza.classList.remove('cinza');
+  if (grayitem) {
+    grayitem.classList.remove('gray');
   }
-  // atribui a classe .cinza ao elemento
-  event.target.classList.add('cinza');
+  // atribui a classe .gray ao elemento
+  event.target.classList.add('gray');
+}
+
+// setItemCompleted(event): atribui/remove a classe .completed a um elemento
+// se o elemento não tem a classe atribuída, atribui; se já tem, retira
+function setItemCompleted(event) {
+  alert('double click');
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 }
 
 // setButtonSubmit(): configura o botão que acrescenta tarefas à lista
@@ -24,8 +35,9 @@ function setButtonSubmit() {
     const tarefa = document.createElement('li'); // cria um elemento li
     tarefa.textContent = input.value; // copia o valor do input para a linha...
     input.value = ''; // ...e apaga o input
-    // acrescenta o event listener do item da lista
+    // acrescenta os event listeners do item da lista
     tarefa.addEventListener('click', setItemGray);
+    tarefa.addEventListener('dblclick', setItemCompleted);
     // obtém o seletor da lista de tarefas e anexa a tarefa como filha
     document.querySelector('#lista-tarefas').appendChild(tarefa);
   });
