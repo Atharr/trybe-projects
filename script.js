d60ff824befc2a503c9056aa426911b9579d53ba
Marcello Pires Alves <marcello.alves@gmail.com>
2021-07-11 21:48:24 -0300

    Requisitos 3 e 4.

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-// setMemeInsert(): configura o input #meme-insert
+// setContainerSize(event): ajusta #meme-image-container ao tamanho da imagem #meme-image
+// isso precisa ser feito no evento onload, pois o tamanho só está definido após a imagem ser carregada!
+function setContainerSize(event) {
+  // obtém o seletor do container #meme-image-container
+  const memeImageContainer = document.getElementById('meme-image-container');
+  // obtém o fator de escala da imagem, caso ela seja menor que o tamanho do container
+  const f = 400 / event.target.naturalWidth;
+  // obtém as dimensões da imagem, ajustando-as caso necessário (fator de escala > 1)
+  const width = (f > 1 ? 400 : event.target.naturalWidth);
+  const height = (f > 1 ? Math.floor(event.target.naturalHeight * f) : event.target.naturalHeight);
+  // ajusta as dimensões do container - isso precisa ser feito após carregar a imagem
+  memeImageContainer.style.width = `${width}px`;
+  memeImageContainer.style.height = `${height}px`;
+}
+
+// setMemeInsert(): configura o input #meme-insert para que a imagem selecionada seja carregada no container
 function setMemeInsert() {
+  // acrescenta o event listener do input #meme-insert
   document.getElementById('meme-insert').addEventListener('change', (event) => {
     // obtém o seletor da img #meme-image
     const memeImage = document.getElementById('meme-image');
     // configura o evento onload de #meme-image para ajustar o container ao tamanho da imagem
-    memeImage.onload = ((img) => {
-      // obtém o seletor do container #meme-image-container
-      const memeImageContainer = document.getElementById('meme-image-container');
-      // ajusta as dimensões do container - isso precisa ser feito ao carregar a imagem
-      memeImageContainer.style.width = `${img.target.naturalWidth}px`;
-      memeImageContainer.style.height = `${img.target.naturalHeight}px`;
-    });
+    memeImage.onload = (setContainerSize);
     // carrega o arquivo como data blob base64 - ref.: https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications
-    const reader = new FileReader();
-    reader.onload = ((a) => (e) => { const x = a; x.src = e.target.result; })(memeImage);
-    reader.readAsDataURL(event.target.files.item(0));
+    const reader = new FileReader(); // cria um novo objeto FileReader
+    reader.onload = ((a) => (e) => { const x = a; x.src = e.target.result; })(memeImage); // transfere o resultado da leitura para memeImage
+    reader.readAsDataURL(event.target.files.item(0)); // lê o arquivo selecionado na forma de string base64
   });
 }
 
-// setTextInput(): configura o input #text-input
+// setTextInput(): configura o input #text-input para que o texto apareça no container da imagem
 function setTextInput() {
   // acrescenta o event listener do input #text-input
   document.getElementById('text-input').addEventListener('input', (event) => {
