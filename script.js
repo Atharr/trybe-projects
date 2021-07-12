d6ca679148be3de36dc8ca4bf7833cde80159776
Marcello Pires Alves <marcello.alves@gmail.com>
2021-07-12 16:48:11 -0300

    Requisito 16.

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// randomClasses(obj): atribui classes aleatórias a um elemento
+function randomClasses(obj) {
+  // array de estilos agrupados em categorias
+  const estilos = [
+    ['newspaper', 'magazine1', 'magazine2'],
+    ['medium', 'big', 'reallybig'],
+    ['rotateleft', 'rotateright'],
+    ['skewleft', 'skewright']];
+  // limpa todas as classes já existentes no objeto
+  while (obj.classList.item(0)) {
+    obj.classList.remove(obj.classList.item(0));
+  }
+  // para cada categoria, acrescenta uma classe aleatória ao objeto
+  estilos.forEach((element) => {
+    obj.classList.add(element[Math.floor(Math.random() * element.length)]);
+  });
+}
+
+// createSpan(texto): configura um elemento span contendo um texto
+function createSpan(texto) {
+  // cria um elemento span
+  const span = document.createElement('span');
+  // atribui ao elemento uma palavra
+  span.textContent = texto;
+  // atribui classes aleatórias ao elemento
+  randomClasses(span);
+  return span;
+}
+
 // setButtonCriar(): configura o botão que gera a carta
 function setButtonCriar() {
   const cartaTexto = document.getElementById('carta-texto'); // obtém o seletor do input #carta-texto
@@ -9,11 +38,7 @@ function setButtonCriar() {
     }
     if (/\S/.test(cartaTexto.value)) { // testa se o texto possui algo além de whitespace
       const texto = cartaTexto.value.split(' '); // separa o texto em um array de palavras
-      texto.forEach((element) => {
-        const span = document.createElement('span'); // cria um elemento span
-        span.textContent = element; // atribui ao elemento uma palavra
-        cartaGerada.appendChild(span); // acrescenta a palavra ao parágrafo #carta-gerada
-      });
+      texto.forEach((element) => { cartaGerada.appendChild(createSpan(element)); }); // acrescenta a palavra ao parágrafo #carta-gerada
     } else {
       cartaGerada.appendChild(document.createTextNode('Por favor, digite o conteúdo da carta.')); // se nada foi digitado, mostra mensagem de erro
     }
