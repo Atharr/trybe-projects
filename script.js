3dd85b1e913262662be6e4d903a14a26039dedaf
Marcello Pires Alves <marcello.alves@gmail.com>
2021-07-21 19:28:22 -0300

    Requisito 21.

--- script.js
+++ script.js
@@ -12,6 +12,57 @@ function setButtonLoginSubmit() {
   });
 }
 
+function getFamily() {
+  const family = document.getElementsByName('family');
+  for (let i = 0; i < family.length; i += 1) {
+    if (family[i].checked) {
+      return family[i].value;
+    }
+  }
+}
+
+function getSubjects() {
+  const subjects = document.getElementsByClassName('subject');
+  let subjectsSelected = '';
+  for (let i = 0; i < subjects.length; i += 1) {
+    if (subjects[i].checked) {
+      subjectsSelected += `${subjects[i].value}, `;
+    }
+  }
+  return subjectsSelected;
+}
+
+function getRate() {
+  const rates = document.getElementsByName('rate');
+  for (let i = 0; i < rates.length; i += 1) {
+    if (rates[i].checked) {
+      return rates[i].value;
+    }
+  }
+}
+
+// setButtonFormSubmit(): configura o botão de submit do form
+function setButtonFormSubmit() {
+  // obtém o seletor do botão e acrescenta o event listener
+  document.getElementById('submit-btn').addEventListener('click', () => {
+    // obtém o seletor do form #evaluation-form
+    const form = document.getElementById('evaluation-form');
+    // obtém os valores dos campos
+    const nome = document.getElementById('input-name').value;
+    const sobrenome = document.getElementById('input-lastname').value;
+    const email = document.getElementById('input-email').value;
+    const casa = document.getElementById('house').value;
+    const familia = getFamily();
+    const materias = getSubjects();
+    const avaliacao = getRate();
+    const textarea = document.getElementById('textarea').value;
+    // preenche o conteúdo do form
+    form.innerHTML = `Nome: ${nome} ${sobrenome}<br>Email: ${email}<br>Casa: ${casa}<br>`;
+    form.innerHTML += `Família: ${familia}<br>Matérias: ${materias}<br>Avaliação: ${avaliacao}<br>`;
+    form.innerHTML += `Observações: ${textarea}<br>`;
+  });
+}
+
 // setCheckAgreement(): configura o checkbox #agreement para habilitar o botão de submit
 function setCheckAgreement() {
   // obtém o seletor do checkbox #agreement e acrescenta o event listener
@@ -36,6 +87,7 @@ function setTextArea() {
 window.onload = () => {
   // configura os event listeners
   setButtonLoginSubmit();
+  setButtonFormSubmit();
   setCheckAgreement();
   setTextArea();
 };
