893eead255410da1ed9adc5ac33d84f4ffe80711
Marcello Pires Alves <marcello.alves@gmail.com>
2021-08-12 14:52:53 -0300

    Requisito 1 - listagem de produtos.

--- script.js
+++ script.js
@@ -40,4 +40,30 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+// getProducts(query): função assíncrona que busca produtos na API do MLB e gera itens na página
+async function getProducts(query) {
+  try {
+    const sectionItems = document.querySelector('.items'); // obtém o seletor da section .items
+    const responseRaw = await fetch(query); // busca no endpoint, recebe resultado 'cru'
+    const responseJSON = await responseRaw.json(); // converte resultado 'cru' em JSON
+    if (!responseJSON.results.length) { // se nenhum resultado foi obtido...
+      throw new Error('Nenhum resultado encontrado!'); // ...envia mensagem de erro
+    }
+    // Cria um elemento para cada produto retornado
+    responseJSON.results.forEach((product) => {
+      const item = { sku: product.id, name: product.title, image: product.thumbnail }; // obtém id, nome e thumbnail do produto
+      sectionItems.appendChild(createProductItemElement(item)); // cria o item e o acrescenta à section .items
+    });
+  } catch (error) { // em caso de erro...
+    alert(`[Erro]: ${error}`); // ...exibe caixa de diálogo com mensagem de erro
+  }
+}
+
+window.onload = () => {
+  // Query a ser usada no projeto
+  const QUERY = 'computador';
+  // Endpoint do MLB
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+  // Busca lista de produtos no endpoint do MLB
+  getProducts(endpoint);
+};
