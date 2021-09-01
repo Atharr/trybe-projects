47fc80b4b9dae3c011f5181e31e7768340bb9ac3
Marcello Pires Alves <marcello.alves@gmail.com>
2021-09-01 00:32:51 -0300

    Requisitos 4 a 6.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,14 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div></div>
+      <div className="movie-list">
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie }/>) }
+      </div>
     );
   }
 }
