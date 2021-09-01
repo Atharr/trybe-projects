48578bf37453694e6235cf13c7576b0ec5dffe0f
Marcello Pires Alves <marcello.alves@gmail.com>
2021-09-01 00:48:07 -0300

    Requisito 9.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,6 +10,7 @@ class MovieCard extends React.Component {
       <div>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
       </div>
     );
   }
