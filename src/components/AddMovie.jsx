810f45506092dee90676604082624059be451779
Marcello Pires Alves <marcello.alves@gmail.com>
2021-09-03 21:49:37 -0300

    Requisito 6.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,31 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class AddMovie extends React.Component {
+  constructor(props) {
+    // sempre devemos invocar o constructor da classe pai primeiro
+    super(props);
+
+    // configura o estado inicial do componente
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    // desestrutura this.state
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
